5bca435674668fe6a8403ddcf4ca2c29a3792836
SevenhuijsenMerlijn <64038833+SevenhuijsenM@users.noreply.github.com>
2020-10-01 15:21:13 +0200

    Added Settings, preparations for messages
    Current settings ( others can be added )
    - Darkmode ( very basic )
    - Orientation
    -  Vibration on or off
    - Temporary button to go to personal settings
    
    Started working on push notifications, currently only works when the app is open or in background, not when fully closed ( swiped ).
    
    Bug:  settings will not be set to default properly

--- app/src/main/java/com/example/pocketalert/MainActivity.java
+++ app/src/main/java/com/example/pocketalert/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.pocketalert;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dropdown_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch ( item.getItemId()) {
+            case R.id.SettingsActivity:
+                Intent settingsIntent = new Intent(this,SettingsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+            case R.id.itemTwo:
+                Toast.makeText(this,"Item 2 Selected",Toast.LENGTH_SHORT);
+                return true;
+            case R.id.itemThree:
+                Toast.makeText(this,"Item 3 Selected",Toast.LENGTH_SHORT);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/example/pocketalert/SettingsActivity.java
+++ app/src/main/java/com/example/pocketalert/SettingsActivity.java
@@ -1,14 +1,52 @@
 package com.example.pocketalert;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class SettingsActivity extends AppCompatActivity {
+    // Make variables for the checkbox of darkmode
+    CheckBox darkModeCheckBox;
+    SharedPreferences appSettingPrefs;
+    Boolean darkModeEnabled;
+    SharedPreferences.Editor sharedPrefsEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        // Dark Mode Checkbox + changes
+        darkModeCheckBox = findViewById(R.id.darkmodeCheckbox);
+        appSettingPrefs = getApplicationContext().getSharedPreferences("AppSettingPrefs",0);
+        darkModeEnabled = appSettingPrefs.getBoolean("NightMode",false);
+        sharedPrefsEdit  = appSettingPrefs.edit();
+        darkModeCheckBox.setChecked(darkModeEnabled);
+
+        darkModeCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (darkModeCheckBox.isChecked()){
+                    enableDarkMode();
+                }
+                else{
+                    disableDarkMode();
+                }
+            }
+        });
+    }
+    void enableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        sharedPrefsEdit.putBoolean("NightMode",true);
+        sharedPrefsEdit.apply();
+    }
+    void disableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        sharedPrefsEdit.putBoolean("NightMode",false);
+        sharedPrefsEdit.apply();
     }
 }
