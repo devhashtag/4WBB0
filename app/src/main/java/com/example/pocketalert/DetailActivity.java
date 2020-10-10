dc069b6af7e4dda1c249d8fee6babe7409399076
Philip Waldman <philip.waldman01@gmail.com>
2020-10-10 14:24:00 +0200

    Added map to details page

--- app/src/main/java/com/example/pocketalert/DetailActivity.java
+++ app/src/main/java/com/example/pocketalert/DetailActivity.java
@@ -9,15 +9,24 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.util.Objects;
+import java.util.Random;
 
 import static com.example.pocketalert.MainActivity.EDIT_DETAILS_ACTIVITY_REQUEST_CODE;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private String id, name, address, phone, email, birthday;
     private TextView idView, nameView, addressView, phoneView, emailView, birthdayView;
     private boolean wasDataUpdated = false;
+    private double latitude, longitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +43,17 @@ public class DetailActivity extends AppCompatActivity {
 
         findViews();
         setTextViews();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
+        // just so the location is different each time. replace with location of device.
+        Random random = new Random();
+        latitude = 51.844975 + random.nextDouble() - 0.5;
+        longitude = 4.927505 + random.nextDouble() - 0.5;
     }
 
     /**
@@ -145,5 +165,12 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         finish();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(latitude, longitude))
+                .title(name));
     }
 }
